32b16a8439f84245a2d2ed041d01f9b03e535f39
VanLandegent <02ethan.vanlandegent@gulllakecs.org>
2024-04-19 13:10:13 -0400

    Fixed Hello World Script

---
+++ Sandbox.js
@@ -0,0 +1,14 @@
+// Programmer: Ethan VanLandegent
+// Team Member: Max Uppal
+// Bolt ID: SB-EC76
+// Date: 4-16-24
+// Program: Hello World - Sandbox
+// Program URL: https://edu.sphero.com/program/16700417/edit
+
+async function startProgram() {
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+}
