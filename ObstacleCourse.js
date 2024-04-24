f5ace7fb068c16b840909dc2be05bdf4026d0c47
VanLandegent <02ethan.vanlandegent@gulllakecs.org>
2024-04-24 13:42:12 -0400

    Comments with Beginning code

---
+++ ObstacleCourse.js
@@ -0,0 +1,10 @@
+// Programmer: Ethan VanLandegent
+// Team Member: Max Uppal
+// Bolt ID: SB-EC76
+// Date: 4-24-24
+// Program: My Obstacle Course - Sandbox
+// Program URL: https://edu.sphero.com/program/16752539/edit
+
+async function startProgram() {
+	// Write code here
+}
