f1f2abca55d19c59e59f0d0cef36b107f8f326bf
VanLandegent <02ethan.vanlandegent@gulllakecs.org>
2024-04-30 14:11:53 -0400

    Blue LED Checkpoint Commit

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -6,5 +6,36 @@
 // Program URL: https://edu.sphero.com/program/16788337/edit
 
 async function startProgram() {
-	// Write code here
+	// Programmer: Maxwell Uppal - Start Text & Audio Checkpoint Commit - Date:
+	await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
+	await speak("Start", true);
+	// Programmer: Ethan VanLandegent - Blue LED Checkpoint Commit - Date:
+	await roll((getHeading() + 0), 50, 3.5);
+	setMainLed({ r: 0, g: 0, b: 255 });
+
+	//Programmer: Maxwell Uppal - Sound One Checkpoint Commit - Date:
+
+
+
+	//Programmer: Ethan VanLandegent - Red LED Checkpoint Commit - Date: 
+
+
+
+	//Programmer: Maxwell Uppal - Sound Two Checkpoint Commit - Date:
+
+
+
+	//Programmer Ethan VanLandegent - Green LED Checkpoint Commit - Date:
+
+
+
+	//Programmer Maxwell Uppal - Purple LED & Sound Three Checkpoint Commit - Date:
+
+
+
+	//Programmer Ethan VanLandegent - Finish Text & Audio Checkpoint Commit - Date:
+
+
+
+
 }
