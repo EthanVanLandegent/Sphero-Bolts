6ec229df8f3f68dd236781ea39ccf1f37c885347
VanLandegent <02ethan.vanlandegent@gulllakecs.org>
2024-04-30 13:28:19 -0400

    Added Beginning Information and Comments and Start Program Jave Script

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+// Programmer: Ethan VanLandegent
+// Team Member: Max Uppal
+// Bolt ID: SB-EC76
+// Date: 4-30-24
+// Program: Maze Competition
+// Program URL: https://edu.sphero.com/program/16788337/edit
+
+async function startProgram() {
+	// Write code here
+}
