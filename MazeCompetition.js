04e52ba821ea0673de4328caec652f7565d25f43
VanLandegent <02ethan.vanlandegent@gulllakecs.org>
2024-05-01 13:44:29 -0400

    Sound 2 chekpoint Commit

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -9,30 +9,47 @@ async function startProgram() {
 	// Programmer: Maxwell Uppal - Start Text & Audio Checkpoint Commit - Date: 4/30/24
 	await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
 	await speak("Start", true);
-	// Programmer: Ethan VanLandegent - Blue LED Checkpoint Commit - Date: 4/30/24
-	await roll((getHeading() + 0), 50, 3.5);
+	
+
+	// Programmer: Ethan Vanlandegent - Blue LED Checkpoint Commit - Date: 4/30/24
+	await roll((getHeading() + 0), 50, 3.65);
+	await delay(1)
 	setMainLed({ r: 0, g: 0, b: 255 });
-	//Programmer: Maxwell Uppal - Sound One Checkpoint Commit - Date: 4/30/24
+	await delay(1)
+
+
+	// Programmer: Maxwell Uppal - Sound One Checkpoint Commit - Date:4/30/24
 	await roll((getHeading() + 90), 50, 2.6);
+	await delay(1)
 	await Sound.ScienceFiction.Blaster.play(true);
-	//Programmer: Ethan VanLandegent - Red LED Checkpoint Commit - Date: 5/1/24
-	await roll((getHeading() + 90), 50, 1.92);
-	await roll((getHeading() + 45), 50, 1.4);
-	//Programmer: Maxwell Uppal - Sound Two Checkpoint Commit - Date:
+	await delay(1)
+	
+
+
+	// Programmer: Ethan Vanlandegent - Red LED Checkpoint Commit - Date:5/1/24 
+	await roll((getHeading() + 90), 50, 1.8);
+	await delay(1)
+	await roll((getHeading() + 43), 50, 1.3);
+	await delay(1)
+
+
+	// Programmer: Maxwell Uppal - Sound Two Checkpoint Commit - Date:5/1/24
+	await roll((getHeading() - 90), 50, .9);
+	await delay(1)
+	await roll((getHeading() - 90), 50, 1.6);
+	await delay(1)
+	await Sound.ScienceFiction.Blaster.play(true);
+
+
+	// Programmer:               - Green LED Checkpoint Commit - Date: 
 
 
 
-	//Programmer Ethan VanLandegent - Green LED Checkpoint Commit - Date:
+	// Programmer:               - Purple LED & Sound Three Checkpoint Commit - Date:
 
 
 
-	//Programmer Maxwell Uppal - Purple LED & Sound Three Checkpoint Commit - Date:
-
-
-
-	//Programmer Ethan VanLandegent - Finish Text & Audio Checkpoint Commit - Date:
-
-
+	// Programmer:               - Finish Text & Audio Commit - Date: 
 
 
 }
